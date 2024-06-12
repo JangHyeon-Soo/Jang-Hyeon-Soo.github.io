a2af04b216a77291209cc787d820e86e9faf93cb
JangHyeon-Soo <jhs513kr@nave.com>
2024-06-12 21:42:21 +0900

    1

---
+++ agency-jekyll-theme-master/웹화면구현_2171040_장현수/js/TitleList.js
@@ -0,0 +1,104 @@
+const Titles =
+    [
+        {
+            name : "Premier League",
+            won: ["1907-08", "1910-11", "1951-52", "1955-56", "1956-57",
+                "1964-65", "1966-67", "1992-93", "1993-94", "1995-96",
+                "1996-97", "1998-99", "1999-00", "2000-01", "2002-03",
+                "2006-07", "2007-08", "2008-09", "2010-11", "2012-13"],
+                Trophy : "웹화면구현_2171040_장현수/Image/PL_Trophy.webp"
+        },
+
+        {
+            name :"EFL Championship",
+            won: [
+                "1935-36", "1974-75"
+            ],
+            Trophy : "웹화면구현_2171040_장현수/Image/EFL_Trophy.webp"
+        },
+
+        {
+            name : "FA Cup",
+            won: [
+                "1908-09", "1947-48", "1962-63", "1976-77", "1982-83",
+                "1984-85", "1989-90", "1993-94", "1995-96", "1998-99",
+                "2003-04", "2015-16", "2023-24"
+            ],
+            Trophy : "웹화면구현_2171040_장현수/Image/FA_Cup.webp"
+        },
+
+        {
+            name: "EFL Cup",
+            won: ["1991-92", "2005-06", "2008-09", "2009-10", "2016-17",
+                "2022-23"],
+                Trophy : "웹화면구현_2171040_장현수/Image/efl_carabao_cup.webp"
+        },
+
+        {
+            name : "FA Community Shield",
+            won: [
+                1908, 1911, 1952, 1956, 1957, 1965, 1967,
+                1977, 1983, 1990, 1993, 1994, 1996, 1997,
+                2003, 2007, 2008, 2010, 2011, 2013, 2016
+            ],
+            Trophy : "웹화면구현_2171040_장현수/Image/Community_Shield.webp"
+        },
+
+        {
+            name : "UEFA Cup Winners Cup",
+             won: [
+                "1990-91"
+            ],
+            Trophy : "웹화면구현_2171040_장현수/Image/UEFA_Cup_Winners_Cup.webp"
+        },
+
+        {
+            name : "UEFA Europa League",
+            won: ["2016-17"],
+            Trophy : "웹화면구현_2171040_장현수/Image/UEFA_Europa_League.svg"
+        },
+
+
+        {
+            
+            name : "Champions League",
+            won: [
+                "1967-68", "1998-99", "2007-08"
+            ],
+            Trophy : "웹화면구현_2171040_장현수/Image/Big_Ear.webp"
+        },
+
+        {
+            name : "UEFA Super Cup", 
+            won: [
+                1991
+            ],
+            Trophy : "웹화면구현_2171040_장현수/Image/UEFA_Super_Cup.svg"
+        },
+        {
+            name : "Intercontinental Cup",
+            won: [1999],
+            Trophy : "웹화면구현_2171040_장현수/Image/IC_Cup.webp"
+        },
+
+        {
+            name : "FIFA Club World Cup",
+            won: [
+                2008
+            ],
+            Trophy : "웹화면구현_2171040_장현수/Image/FIFA_Club_World_Cup.svg"
+        }
+
+
+
+
+
+    ];
+
+
+
+
+
+
+
+
